e80623beda946ddb9db6e36dc987046b9faf83dd
Okirshen <okirshen@gmail.com>
2020-12-24 01:21:21 +0200

    space

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import { Context, Middleware } from 'https://deno.land/x/oak/mod.ts';
 
 export const loggit: Middleware = async (ctx: Context, next) => {
 	await next();
+
 	console.log(
 		`${ctx.request.method} - ${ctx.response.status}: ${ctx.request.url}`
 	);
